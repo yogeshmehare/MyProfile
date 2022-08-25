d0b8496beab4e36ce806dffcb661e65ad928f207
yogesh <yogeshmehare124@gmail.com>
2022-08-25 09:52:26 +0530

    Added Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
     return (
       <div>
         <p>Yogesh Mehare</p>
+        <h1 className="text-5xl font-bold underline">Hello world!</h1>
       </div>
     );
   }
